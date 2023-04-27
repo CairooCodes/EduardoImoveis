90b21379a51b96007d83ba44290f88590efb3f81
tiagoCB41 <thiagocarvalho041@gmail.com>
2023-04-27 00:41:19 -0300

    fix search in mobile

--- components/navbar.php
+++ components/navbar.php
@@ -50,6 +50,16 @@
       <div class="lg:hidden px-4 space-x-2 flex justify-center">
         <ul class="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0">
           <li>
+          <div class="h-20 relative w-full mx-auto text-white">
+        <input style="background: rgba(45,45,45,0.8);" class="h-full w-full bg-white text-white px-16 pr-24 text-md focus:outline-none" type="search" name="search" placeholder="BUSQUE SEU IMÓVEL">
+        <button type="submit" class="absolute right-0 top-0 mt-7 mr-8">
+          <svg class="text-gray-100 h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
+            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg>
+        </button>
+        </div>
+          </li>
+          <li>
             <a href="home" class="block py-2 pl-3 pr-4 text-color2 rounded lg:p-0" aria-current="page">HOME</a>
           </li>
           <li>
