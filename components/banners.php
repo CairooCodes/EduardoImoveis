08201362fb7edd4f114718b56bd5de26cda25eeb
Alex-Brendon <alexbrendon11.dev@gmail.com>
2023-05-09 02:46:23 -0300

    add swiper banners eduardo imoveis

--- components/banners.php
+++ components/banners.php
@@ -1,24 +1,23 @@
 <section class="swiper swiper_banners relative mt-20">
   <div class="swiper-wrapper">
-    <?php
-    $stmt = $pdo->prepare("SELECT * FROM banners");
-    $stmt->execute();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      extract($row);
-    ?>
-      <div class="swiper-slide">
-      <?php
-        if (!empty($row['img'])) {
-            $img = base64_encode($row['img']);
-            echo "<img class='w-full' src='data:image/jpeg;base64," . $img . "'>";
-        }
-        ?>
-      </div>
-    <?php
-    }
-    ?>
+    <div class="swiper-slide">
+      <img class="w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="./assets/img/logo.png" alt="feature image 2">
+      <h3 class="text-2xl font-bold text-center">
+        <div class="swiper mySwiper pt-10">
+          <div class="swiper-wrapper pb-10">
+            <div class="swiper-slide h-60 lg:h-32 flex p-10 items-center shadow rounded-b-xl">
+              <img class="mb-2 w-full" src="./assets/img/logo.png">
+            </div>
+            <div class="swiper-slide h-60 lg:h-32 flex p-10 items-center shadow rounded-b-xl">
+              <img class="mb-2 w-full" src="./assets/img/metro.png">
+            </div>
+            <div class="swiper-slide h-60 lg:h-32 flex p-10 items-center shadow rounded-b-xl">
+              <img class="mb-2 w-full" src="./assets/img/quarto.png">
+            </div>
+          </div>
+          <div class="swiper-pagination swiper-pacotes"></div>
+        </div>
+      </h3>
+    </div>
   </div>
-  <div class="swiper-button-next text-white"></div>
-  <div class="swiper-button-prev text-white"></div>
-  <div class="swiper-pagination"></div>
 </section>
